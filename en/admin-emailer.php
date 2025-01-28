d35f124e8cab99988432efb0f2b2778b21428b27
Russell Maier <russ@earthen.io>
2025-01-28 10:43:49 +0700

    webhook email handling

--- en/admin-emailer.php
+++ en/admin-emailer.php
@@ -104,12 +104,12 @@ if (!empty($date_registered)) {
 $body = "
 Hi there $first_name,<br><br>
 GoBrik has been totally revamped for 2025! <br><br>
-We've removed our reliance on Google, Facebook and Amazon services and need you to re-activate your new account with our new Buwana authentication protocol.<br><br>
+To align with our ecological principles, we've removed our reliance on Google, Facebook and Amazon services.  We are using our own database and server now and need you to re-activate your new account with our new Buwana authentication protocol-- a new account system for GoBrik and other regenerative apps ()read more about our 2025 big tech transition <a href=\"https://earthen.io/gobrik-2025-accounts-ditching-big-tech-2/\">here</a><br><br>
 You've been with us since you registered on $date_registered.<br><br>";
 
-if ($ecobricks_made > 0) {
-    $body .= "In your old account, we have your $ecobricks_made ecobricks and $brk_balance Brikcoins.<br><br>";
-}
+
+$body .= "In your old account, we have your $ecobricks_made ecobricks and the $brk_balance Brikcoins you earned.<br><br>";
+
 
 $body .= "
 Your ecobricking work was a great service to your local $city_txt ecology and your $region_txt bioregion of $country_txt, Earth.<br><br>
