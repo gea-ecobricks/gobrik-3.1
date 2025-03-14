03130a1c1cfb74131a7ddae4e3380ac72d5d7718
Russell Maier <russ@earthen.io>
2025-03-14 10:36:51 +0700

    webhook email handling

--- en/register.php
+++ en/register.php
@@ -87,7 +87,8 @@ if ($result->num_rows > 0) {
     $registration_scope = htmlspecialchars($row['registration_scope'], ENT_QUOTES, 'UTF-8');
     $location_full = htmlspecialchars($row['location_full'], ENT_QUOTES, 'UTF-8');
     $training_summary = nl2br(htmlspecialchars($row['training_summary'], ENT_QUOTES, 'UTF-8'));
-    $training_agenda = nl2br(htmlspecialchars($row['training_agenda'], ENT_QUOTES, 'UTF-8'));
+    $allowed_tags = '<b><i><u><strong><em><p><br><ul><li><ol>';
+    $training_summary = strip_tags($_POST['training_summary'], $allowed_tags);
     $training_success = nl2br(htmlspecialchars($row['training_success'], ENT_QUOTES, 'UTF-8'));
     $training_challenges = nl2br(htmlspecialchars($row['training_challenges'], ENT_QUOTES, 'UTF-8'));
     $training_lessons_learned = nl2br(htmlspecialchars($row['training_lessons_learned'], ENT_QUOTES, 'UTF-8'));
