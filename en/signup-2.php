8a1980cf476508e5cef493ab63a9ab74fe554c2f
Russell Maier <russ@earthen.io>
2025-03-17 11:47:26 +0700

    webhook email handling

--- en/signup-2.php
+++ en/signup-2.php
@@ -101,7 +101,7 @@ https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
             <!--SIGNUP FORM-->
             <form id="password-confirm-form" method="post" action="signup_process.php?id=<?php echo htmlspecialchars($buwana_id); ?>">
 
-                <div class="form-item" id="last-name" class="user_lastname">
+                <div class="form-item" id="last-name" class="user_lastname" style="display:none;">
                     <label for="last_name" data-lang-id="011b-last-name">Now what is your last name?</label><br>
                     <input type="text" id="human_check" class="required" placeholder="Your last name...">
                     <p class="form-caption" data-lang-id="011b-required" style="color:red">*This field is required.</p>
