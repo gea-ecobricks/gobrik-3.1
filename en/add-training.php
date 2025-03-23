dfd8789c00ecf5edb535bc122daae56f6f16fed7
Russell Maier <russ@earthen.io>
2025-03-23 11:01:17 +0700

    webhook email handling

--- en/add-training.php
+++ en/add-training.php
@@ -32,7 +32,7 @@ if (!$gea_status || stripos($gea_status, 'trainer') === false) {
 require_once '../buwanaconn_env.php';
 $user_continent_icon = getUserContinent($buwana_conn, $buwana_id);
 $user_location_watershed = getWatershedName($buwana_conn, $buwana_id);
-$user_location_full = getUserFullLocation($buwana_conn, $buwana_id);
+$user_training_location = getUserFullLocation($buwana_conn, $buwana_id);
 $user_community_name = getCommunityName($buwana_conn, $buwana_id);
 $first_name = getFirstName($buwana_conn, $buwana_id);
 
@@ -67,7 +67,7 @@ if ($editing) {
     $stmt_fetch->bind_param("i", $training_id);
     $stmt_fetch->execute();
     $stmt_fetch->bind_result($training_title, $lead_trainer, $country_id, $training_date, $no_participants,
-                            $training_type, $briks_made, $avg_brik_weight, $latitude, $longitude, $location_full,
+                            $training_type, $briks_made, $avg_brik_weight, $latitude, $longitude, $training_location,
                             $training_summary, $training_agenda, $training_success, $training_challenges,
                             $training_lessons_learned, $youtube_result_video, $moodle_url, $ready_to_show, $featured_description, $community_id);
     $stmt_fetch->fetch();
@@ -195,7 +195,7 @@ if ($editing) {
     $sql = "UPDATE tb_trainings SET
         training_title=?, lead_trainer=?, country_id=?, training_date=?,
         no_participants=?, training_type=?, briks_made=?, avg_brik_weight=?,
-        location_lat=?, location_long=?, location_full=?, training_summary=?, training_agenda=?,
+        location_lat=?, location_long=?, training_location=?, training_summary=?, training_agenda=?,
         training_success=?, training_challenges=?, training_lessons_learned=?,
         youtube_result_video=?, moodle_url=?, ready_to_show=?, featured_description=?, community_id=?
         WHERE training_id=?";
@@ -203,7 +203,7 @@ if ($editing) {
     $stmt = $gobrik_conn->prepare($sql);
     $stmt->bind_param("ssisisiiiddssssssssssi",
         $training_title, $lead_trainer, $country_id, $training_date, $no_participants,
-        $training_type, $briks_made, $avg_brik_weight, $latitude, $longitude, $location_full,
+        $training_type, $briks_made, $avg_brik_weight, $latitude, $longitude, $training_location,
         $training_summary, $training_agenda, $training_success, $training_challenges,
         $training_lessons_learned, $youtube_result_video, $moodle_url, $ready_to_show,
         $featured_description, $community_id, $training_id
@@ -213,14 +213,14 @@ if ($editing) {
     $sql = "INSERT INTO tb_trainings
         (training_title, lead_trainer, country_id, training_date, no_participants,
         training_type, briks_made, avg_brik_weight, location_lat, location_long,
-        location_full, training_summary, training_agenda, training_success, training_challenges,
+        training_location, training_summary, training_agenda, training_success, training_challenges,
         training_lessons_learned, youtube_result_video, moodle_url, ready_to_show, featured_description, community_id)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     $stmt = $gobrik_conn->prepare($sql);
     $stmt->bind_param("ssisisiiiddssssssssssi",
         $training_title, $lead_trainer, $country_id, $training_date, $no_participants,
-        $training_type, $briks_made, $avg_brik_weight, $latitude, $longitude, $location_full,
+        $training_type, $briks_made, $avg_brik_weight, $latitude, $longitude, $training_location,
         $training_summary, $training_agenda, $training_success, $training_challenges,
         $training_lessons_learned, $youtube_result_video, $moodle_url, $ready_to_show, $featured_description, $community_id
     );
@@ -457,7 +457,7 @@ $og_image = !empty($feature_photo1_main) ? $feature_photo1_main : "https://gobri
 <!--        <div class="form-item">
             <label for="location_address">Training Location:</label><br>
             <input type="text" id="location_address" name="location_address" placeholder="Start typing your town..." required
-                value="<?php echo htmlspecialchars($location_full ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+                value="<?php echo htmlspecialchars($training_location ?? '', ENT_QUOTES, 'UTF-8'); ?>">
         </div>
         <div id="location-error-required" class="form-field-error" data-lang-id="000-field-required-error">This field is required.</div>-->
     <div class="form-item">
