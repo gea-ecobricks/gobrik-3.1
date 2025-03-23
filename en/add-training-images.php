b88e59b0043c6e64a10fd97cff60227cf04e1d72
Russell Maier <russ@earthen.io>
2025-03-23 13:10:35 +0700

    webhook email handling

--- en/add-training-images.php
+++ en/add-training-images.php
@@ -200,11 +200,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['training_id'])) {
 <!DOCTYPE html>
 <HTML lang="en">
 <HEAD>
-    <META charset="UTF-8">
-    <?php $lang='en';?>
-    <?php $version='2.4';?>
-    <?php $page='add-training-images';?>
 
+    <META charset="UTF-8">
+
+
+<title><?php echo !empty($training_title) ? $training_title : 'Log your Training Report'; ?></title>
+<meta name="keywords" content="GEA Registration, Community, Event, Webinar, Course">
+<meta name="description" content="<?php echo !empty($training_type) && !empty($lead_trainer) && !empty($training_date)
+    ? "Log the $training_type led by $lead_trainer on $training_date on the GEA reporting system. Reports will be shared on the front page of Ecobricks.org."
+    : "Log your GEA workshop. Reports will be featured on the front page of Ecobricks.org and shareable on social media."; ?>">
+
+<!-- Facebook Open Graph Tags for social sharing -->
+<meta property="og:url" content="https://www.gobrik.com/<?php echo $lang; ?>/add-report.php">
+<meta property="og:type" content="website">
+<meta property="og:title" content="<?php echo !empty($training_title) ? $training_title : 'Log your Training Report'; ?>">
+<meta property="og:description" content="<?php echo !empty($training_type) && !empty($lead_trainer) && !empty($training_date)
+    ? "Log the $training_type led by $lead_trainer on $training_date on the GEA reporting system. Reports will be shared on the front page of Ecobricks.org."
+    : "Log your GEA workshop. Reports will be featured on the front page of Ecobricks.org and shareable on social media."; ?>">
+
+<!-- Default image in case no feature image is available -->
+<?php
+$og_image = !empty($feature_photo1_main) ? $feature_photo1_main : "https://gobrik.com/svgs/shanti.svg";
+?>
+<meta property="og:image" content="<?php echo $og_image; ?>">
+<meta property="fb:app_id" content="1781710898523821">
+<meta property="og:image:width" content="1000">
+<meta property="og:image:height" content="1000">
+<meta property="og:image:alt" content="<?php echo !empty($training_title) ? $training_title : 'GEA Trainer in action'; ?>">
+<meta property="og:locale" content="en_GB">
+
+<meta property="article:modified_time" content="<?php echo date("c"); ?>">
+
+<meta name="author" content="GoBrik.com">
+<meta property="og:type" content="page">
+<meta property="og:site_name" content="GoBrik.com">
+<meta property="article:publisher" content="https://web.facebook.com/ecobricks.org">
+<meta property="og:image:type" content="image/png">
+<meta name="author" content="GoBrik.com">
+
+<!--PART 5 TOP DECORATION-->
     <?php require_once ("../includes/add-training-inc.php");?>
 
     <div class="splash-content-block"></div>
