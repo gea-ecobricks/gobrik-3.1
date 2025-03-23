46b53e4f5f583bfa6f976c55f951f8db0e8fe745
Russell Maier <russ@earthen.io>
2025-03-23 17:12:36 +0700

    webhook email handling

--- en/add-training-images.php
+++ en/add-training-images.php
@@ -6,7 +6,7 @@ require_once '../earthenAuth_helper.php'; // Authentication helper
 
 // PART 1: Set page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.56';
+$version = '0.57';
 $page = 'add-training-images';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 
@@ -283,7 +283,7 @@ $og_image = !empty($feature_photo1_main) ? $feature_photo1_main : "https://gobri
                     $photo_tmb_var = "training_photo{$i}_tmb";
                 ?>
                 <div class="form-item">
-                    <label for="training_photo<?php echo $i; ?>_main></label><br>
+
                     <p data-lang-id="select-photo-<?php echo $i; ?></p>
 
                     <!-- ✅ Show existing image if available -->
