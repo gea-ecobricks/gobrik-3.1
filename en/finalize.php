9a57fd255e7f5c296515fd2b6276059ce6b83a89
Russell Maier <russ@earthen.io>
2025-04-05 22:52:28 +0700

    webhook email handling

--- en/finalize.php
+++ en/finalize.php
@@ -336,7 +336,7 @@ $current_lang_dir = basename(dirname($_SERVER['SCRIPT_NAME']));
         <input type="submit" id="submit-button" value="All done!" class="submit-button enabled">
 
     </div>
-<p class="form-caption" data-lang-id="022">Complete your Buwana account registration and login.</p>
+<p class="form-caption" data-lang-id="022" style="text-align: center;">🐵 Complete your Buwana account registration and login.</p>
 
 </form>
 
