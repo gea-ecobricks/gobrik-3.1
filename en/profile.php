26f039fbddc36aa131e4d01249b30240f0c999d8
Russell Maier <russ@earthen.io>
2025-04-06 15:31:44 +0700

    webhook email handling

--- en/profile.php
+++ en/profile.php
@@ -442,10 +442,18 @@ echo '<!DOCTYPE html>
     <h2 data-lang-id="028-delete-heading">Delete Your Account</h2>
     <p data-lang-id="029-delete-warning">Warning: Deleting your account will permanently remove all your data and cannot be undone.</p>
 
-    <form id="delete-account-form" method="post">
+<!-- DELETE ACCOUNT FORM -->
+            <form id="delete-account-form" method="post" action="../api/delete_accounts.php?id=<?php echo htmlspecialchars($ecobricker_id); ?>">
+                <div style="text-align:center;width:100%;margin:auto;margin-top:10px;margin-bottom:10px;">
+                    <button type="button" class="submit-button delete" onclick="confirmDeletion()" data-lang-id="0010-delete-button">Delete My Account</button>
+                </div>
+
+            </form>
+
+    <!--<form id="delete-account-form" method="post">
     <button type="button" onclick="confirmDeletion('<?php echo htmlspecialchars($buwana_id); ?>', '<?php echo htmlspecialchars($lang); ?>')" class="submit-button delete" aria-label="Delete Account" data-lang-id="030-delete-my-account-button">Delete My Account</button>
 
-</form>
+</form>-->
 
 
 
@@ -955,7 +963,15 @@ function selectEarthlingEmoji(emoji) {
 }
 </script>
 
-
+<script>
+function confirmDeletion() {
+    if (confirm("Are you certain you wish to delete your account? This cannot be undone.")) {
+        if (confirm("Ok. We will delete your account! Note that this does not affect ecobrick data that has been permanently archived in the brikchain. If you have a Buwana account and/or a subscription to our Earthen newsletter it will also be deleted.")) {
+            document.getElementById('delete-account-form').submit();
+        }
+    }
+}
+</script>
 </body>
 
 </html>
