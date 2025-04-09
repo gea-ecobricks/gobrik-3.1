8192c7430d9f34222a1dc2b371dc62e60a2002b0
Russell Maier <russ@earthen.io>
2025-04-09 23:40:47 +0700

    webhook email handling

--- en/dashboard.php
+++ en/dashboard.php
@@ -19,7 +19,8 @@ if ($is_logged_in) {
     require_once '../buwanaconn_env.php';
 
     // Fetch the user's location data
-    $user_continent_icon = getUserContinent($buwana_conn, $buwana_id);
+    $earthling_emoji = '';
+    $continent_icon = getUserContinent($buwana_conn, $buwana_id, $earthling_emoji);
     $user_location_watershed = getWatershedName($buwana_conn, $buwana_id);
     $user_location_full = getUserFullLocation($buwana_conn, $buwana_id);
     $gea_status = getGEA_status($buwana_id);
