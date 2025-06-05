45a253e5c0e383541b653610af3b8c60aed28e54
GEA Admin <64298789+gea-ecobricks@users.noreply.github.com>
2025-06-05 17:07:18 +0700

    Refine launch toggle layout and photo labels

--- en/launch-training.php
+++ en/launch-training.php
@@ -385,13 +385,13 @@ if (!empty($community_id)) {
     </div>
 
     <div class="form-item">
-        <label for="feature_photo2_main">Set a secondary training photo</label><br>
+        <label for="feature_photo2_main">Set a second training feature photo</label><br>
         <input type="file" id="feature_photo2_main" name="feature_photo2_main" class="form-field-style">
         <p class="form-caption">This image will be visible in the training registration page.</p>
     </div>
 
     <div class="form-item">
-        <label for="feature_photo3_main">Set a third training photo</label><br>
+        <label for="feature_photo3_main">Set a third training feature photo</label><br>
         <input type="file" id="feature_photo3_main" name="feature_photo3_main" class="form-field-style">
         <p class="form-caption">This image will also be visible in the training registration page.</p>
     </div>
@@ -413,17 +413,17 @@ if (!empty($community_id)) {
     </div>
 
     <!-- Ready to Show -->
-    <div class="form-item form-row">
-        <div class="toggle-left">
+    <div class="form-row ready-field">
+        <div class="toggle-info">
+            <label class="toggle-description" for="ready_to_show" data-lang-id="024-title-show">Launch this training</label>
+            <p class="form-caption" data-lang-id="022-training-show">Is this training ready to be displayed on ecobricks.org?  If so, we'll post the completed workshop for to the live feed of GEA trainings.  Don't worry you can always come back here to edit the live listing!</p>
+        </div>
+        <div class="toggle-slider">
             <label class="toggle-switch">
                 <input type="checkbox" id="ready_to_show" name="ready_to_show" value="1"
                        <?php echo (isset($ready_to_show) && $ready_to_show) ? 'checked' : ''; ?>>
                 <span class="slider"></span>
             </label>
-        </div>
-        <div class="toggle-right">
-            <label class="toggle-description" for="ready_to_show" data-lang-id="024-title-show">Launch this training</label>
-            <p class="form-caption" data-lang-id="022-training-show">Is this training ready to be displayed on ecobricks.org?  If so, we'll post the completed workshop for to the live feed of GEA trainings.  Don't worry you can always come back here to edit the live listing!</p>
         </div>
     </div>
 <input type="hidden" id="training_id" name="training_id" value="<?php echo htmlspecialchars($training_id ?? '', ENT_QUOTES, 'UTF-8'); ?>">
