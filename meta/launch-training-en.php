abd41738a1a00d1de803d4a606b97fbb0dc89b22
Russell Maier <russ@earthen.io>
2025-06-08 14:12:03 +0700

    Fix Register look

--- meta/launch-training-en.php
+++ meta/launch-training-en.php
@@ -1,27 +1,23 @@
 
-<title><?php echo !empty($training_title) ? $training_title : 'Launch your GEA training!'; ?></title>
-<meta name="keywords" content="GEA Registration, Community, Event, Webinar, Course">
-<meta name="description" content="<?php echo !empty($training_type) && !empty($lead_trainer) && !empty($training_date)
-    ? "Launch, list and manage the $training_type led by $lead_trainer on $training_date on GoBrik. Trainings will be featuring publicly for user registration once launched."
-    : "Launch your GEA training, event, webinar or workshop. Trainings will be featured on the front page of Ecobricks.org and shareable on social media."; ?>">
+<title>Launch your GEA training</title>
+<meta name="keywords" content="GEA, course, launch, training, workshop, webinar, event, GEA Registration, Community, Event, Webinar, Course">
+<meta name="description" content="Launch, list and manage your workshops, events and trainings on GoBrik. Trainings will be featuring publicly for user registration once launched.">
 
 <!-- Facebook Open Graph Tags for social sharing -->
 <meta property="og:url" content="https://www.gobrik.com/<?php echo $lang; ?>/launch-training.php">
 <meta property="og:type" content="website">
-<meta property="og:title" content="<?php echo !empty($training_title) ? $training_title : 'Launch your GEA training!'; ?>">
-<meta property="og:description" content="<?php echo !empty($training_type) && !empty($lead_trainer) && !empty($training_date)
-                                             ? "Launch, list and manage the $training_type led by $lead_trainer on $training_date on GoBrik. Trainings will be featuring publicly for user registration once launched."
-                                             : "Launch your GEA training, event, webinar or workshop. Trainings will be featured on the front page of Ecobricks.org and shareable on social media."; ?>">
+<meta property="og:title" content="Launch your GEA training">
+<meta property="og:description" content="Launch, list and manage your workshops, events and trainings on GoBrik. Trainings will be featuring publicly for user registration once launched.">
 
 <!-- Default image in case no feature image is available -->
 <?php
 $og_image = !empty($feature_photo1_main) ? $feature_photo1_main : "https://gobrik.com/svgs/shanti.svg";
 ?>
-<meta property="og:image" content="<?php echo $og_image; ?>">
+<meta property="og:image" content="https://gobrik.com/pngs/launch-training-banner.png">
 <meta property="fb:app_id" content="1781710898523821">
-<meta property="og:image:width" content="1000">
-<meta property="og:image:height" content="1000">
-<meta property="og:image:alt" content="<?php echo !empty($training_title) ? $training_title : 'GEA Trainer in action'; ?>">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="613">
+<meta property="og:image:alt" content="A GEA Trainer in green action">
 <meta property="og:locale" content="en_GB">
 
 <meta property="article:modified_time" content="<?php echo date("c"); ?>">
