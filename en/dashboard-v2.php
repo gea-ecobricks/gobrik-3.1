1c8fdfa1dafcd6809b459486ccc545a6f4570bbc
ubuntusitubondo <russ@earthen.io>
2025-12-07 16:12:13 +0700

    local dashboard v2 refinement

--- en/dashboard-v2.php
+++ en/dashboard-v2.php
@@ -3,7 +3,7 @@ require_once '../earthenAuth_helper.php'; // 🌿 Optional helper functions
 
 // 🌍 Set up page environment
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '3.3';
+$version = '3.4';
 $page = 'dashboard';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 
