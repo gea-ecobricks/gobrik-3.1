a24a77005fc5d8ded21829b8a0d3ec44d5a370b5
ubuntusitubondo <russ@earthen.io>
2026-01-19 17:21:05 +0700

    local dashboard v2 refinement

---
+++ meta/edit-project-en.php
@@ -0,0 +1,24 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Edit your ecobrick project</title>
+
+<meta name="keywords" content="edit, post, share, submit, ecobrick, project">
+
+<meta name="description" content="Update your ecobrick project post on ecobricks.org">	
+
+<meta name="robots" content="noindex">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://ecobricks.org<?php include 'lang.php';?>/edit-project.php">
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Edit your ecobrick project">
+  	<meta property="og:description"   content="Update your ecobrick project post on ecobricks.org">
+  	<meta property="og:image"         content="https://ecobricks.org/webp/build-blue-450px.webp">
+  	<meta property="og:image:width" content="450px" >
+	<meta property="og:image:height" content="450px" >
+  	<meta property="og:image:alt"     content="Ecobrick projects">
+	  <meta property="og:locale" content="<?php include 'lang.php';?>_<?php include 'lang.php';?>" >
+
+	  <meta name="author" content="Ecobricks.org">
+  
